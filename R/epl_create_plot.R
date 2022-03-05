4750bab73f277070259887324249ac7da2fe0af8
Writer <typer.writer.io@gmail.com>
2022-03-05 12:50:30 -0700

    Update epl_create_plot.R

--- R/epl_create_plot.R
+++ R/epl_create_plot.R
@@ -33,3 +33,63 @@ epl_plot <- ggplot(current_table2, aes(factor(match), team3, fill = count)) +
 
 ggsave("graphs/heatmap.png", plot = epl_plot,
        width = 8, height = 6, units = "in")
+
+
+tidy_fun <- function(.data) {
+  data <- .data %>% 
+  pivot_longer(!team, names_to = "match", values_to = "count") %>% 
+  mutate(match = as.numeric(match)) %>% 
+  mutate(team3 = fct_inorder(team, ordered = TRUE))
+
+  data
+}
+
+plot_fun <- function(.data) {
+  
+epl_plot <- ggplot(.data, aes(factor(match), team3, fill = count)) +
+  geom_tile(color = "white",
+            lwd = .5,
+            linetype = 1,
+             height = 0.4) +
+  coord_fixed() +
+  theme_minimal() +
+  scale_y_discrete(limits = rev(levels(current_table2$team3))) +
+  scale_x_discrete(position = "top") +
+  xlab("Match") +
+  ylab("") +
+  theme(legend.position="none") +
+  theme(text=element_text(family="mono")) +
+  labs(caption = paste("Updated:", Sys.time(), Sys.timezone())) + 
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank()) 
+
+
+epl_plot
+}
+
+
+current_table_simple <- current_table %>% 
+  mutate_if(is.numeric, funs(case_when(
+      . < 0 ~ -1,
+      . == 0 ~ 0,
+      . > 0 ~ 1
+    )))
+
+
+
+epl_plot2 <- tidy_fun(current_table_simple) %>% 
+  plot_fun()  + 
+  scale_fill_gradient2(low = "#FFD0C0",
+                       mid = "#F5F5F5",
+                       high = "#A0BCD6", 
+                       na.value = "white"
+)
+
+
+
+ggsave("graphs/heatmap2.png", plot = epl_plot2,
+       width = 8, height = 6, units = "in")
+
